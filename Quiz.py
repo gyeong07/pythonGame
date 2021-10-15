383d94e7b4c20f6f403af75ca55303f21bd3fc2f
gyeong07 <vlem4646@gmail.com>
2021-10-15 20:45:37 +0900

    Create Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,4 +1,19 @@
+# Quiz) 하늘에서 떨어지는 똥 피하기 게임
+
+# [게임 조건]
+# 1. 캐릭터는 화면 가장 아래에 위치, 좌우로만 이동 가능
+# 2. 똥은 화면 가장 위에서 떨어짐. x 좌표는 매번 랜덤으로 설정.
+# 3. 캐릭터가 똥을 피하면 다음 똥이 다시 떨어짐.
+# 4. 캐릭터가 똥과 충돌하면 게임 종료.
+# 5. FPS는 30으로 고정
+
+# [게임 이미지]
+# 1. 배경: 640 * 480 (세로 가로) - background.png
+# 2. 캐릭터: 70 * 70 - character.png
+# 3. 똥: 70 * 70 - enemy.png
+
 import pygame
+import random
 
 pygame.init()  # 초기화 (반드시 필요)
 
@@ -26,11 +41,8 @@ character_x_pos = (screen_width / 2) - \
     (character_width / 2)  # 화면 가로의 절반 크기에 해당하는 곳에 위치
 character_y_pos = screen_height - character_height  # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
 
-# 이동할 좌표
+# 이동
 to_x = 0
-to_y = 0
-
-# 이동 속도
 character_speed = 0.5
 
 # 적 enemy 캐릭터
@@ -39,26 +51,15 @@ enemy = pygame.image.load(
 enemy_size = enemy.get_rect().size  # 이미지의 크기를 구해옴
 enemy_width = enemy_size[0]  # 캐릭터의 가로 크기
 enemy_height = enemy_size[1]  # 캐릭터의 세로 크기
-enemy_x_pos = (screen_width / 2) - \
-    (enemy_width / 2)  # 화면 가로의 절반 크기에 해당하는 곳에 위치
-# 화면 세로 크기 가장 아래에 해당하는 곳에 위치
-enemy_y_pos = (screen_height / 2) - (enemy_height / 2)
-
-
-# 폰트 정의
-game_font = pygame.font.Font(None, 40)  # 폰트 객체 생성(폰트, 크기)
-
-# 총 시간
-total_time = 10
-
-# 시작 시간 정보
-start_ticks = pygame.time.get_ticks()  # 시작 tick을 받아옴
+enemy_x_pos = random.randint(0, screen_width - enemy_width)
+enemy_y_pos = 0
+enemy_speed = 10
 
 
 # 이벤트 루프
 running = True  # 게임이 진행중인가?
 while running:
-    dt = clock.tick(60)  # 게임화면의 초당 프레임 수를 설정
+    dt = clock.tick(30)  # 게임화면의 초당 프레임 수를 설정
 
     for event in pygame.event.get():  # 어떤 이벤트가 발생하였는가?
         if event.type == pygame.QUIT:  # 창이 닫히는 이벤트가 발생하였는가?
@@ -69,19 +70,12 @@ while running:
                 to_x -= character_speed
             elif event.key == pygame.K_RIGHT:
                 to_x += character_speed
-            elif event.key == pygame.K_UP:
-                to_y -= character_speed
-            elif event.key == pygame.K_DOWN:
-                to_y += character_speed
 
         if event.type == pygame.KEYUP:  # 방향키를 떼면 멈춤
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                to_y = 0
 
     character_x_pos += to_x * dt
-    character_y_pos += to_y * dt
 
     # 가로 경계값 처리
     if character_x_pos < 0:
@@ -89,11 +83,11 @@ while running:
     elif character_x_pos > screen_width - character_width:
         character_x_pos = screen_width - character_width
 
-    # 세로 경계값 처리
-    if character_y_pos < 0:
-        character_y_pos = 0
-    elif character_y_pos > screen_height - character_height:
-        character_y_pos = screen_height - character_height
+    enemy_y_pos += enemy_speed
+
+    if enemy_y_pos > screen_height:
+        enemy_y_pos = 0
+        enemy_x_pos = random.randint(0, screen_width - enemy_width)
 
     # 충돌 처리를 위한 rect 정보 업데이트
     character_rect = character.get_rect()
@@ -114,21 +108,6 @@ while running:
     screen.blit(background, (0, 0))  # 배경 그리기
     screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
     screen.blit(enemy, (enemy_x_pos, enemy_y_pos))  # 적 그리기
-
-    # 타이머
-    # 경과 시간 계산
-    elapsed_time = (pygame.time.get_ticks() - start_ticks) / \
-        1000  # 경과 시간(ms)을 1000으로 나누어 초 단위로 표시
-
-    timer = game_font.render(
-        str(int(total_time-elapsed_time)), True, (255, 255, 255))
-    # 출력할 글자, True, 글자 색상
-    screen.blit(timer, (10, 10))
-
-    # 만약 시간 <= 0 이하이면 게임 종료
-    if total_time - elapsed_time <= 0:
-        print("타임아웃")
-        running = False
 
     pygame.display.update()  # 게임화면을 다시 그리기!
 
